93b280369b63a0f5e3b52361af93db4e006da941
Jahangir Alam <jahangir.2012@outlook.com>
2024-06-04 20:14:01 +0600

    db connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("Hello Node")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb",
+});
+
+
+try {
+  connection.connect();
+
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+
+  connection.query(sql, (err, rows, fields) => {
+    if (err) throw err;
+
+    console.log("1 record inserted");
+  });
+
+  connection.end();
+} catch (error) {
+  console.log("error", error);
+  throw error;
+}
